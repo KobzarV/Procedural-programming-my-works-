29da4a9969a8966343525f7c086f15114c5ed349
_Kobzaryk_ <114069847+KobzarV@users.noreply.github.com>
2023-01-03 23:59:25 +0200

    Update Хрестики-нулики.cpp

--- basic_exercises/Хрестики-нулики.cpp
+++ basic_exercises/Хрестики-нулики.cpp
@@ -9,9 +9,9 @@ char Player1[50], Player2[50];
 char cells[9] = { '1','2','3','4','5','6','7','8','9' };
 
 void print_field() {
-    system("cls");  // очищає поле (для зручності)
+    system("cls");  // РѕС‡РёС‰Р°С” РїРѕР»Рµ (РґР»СЏ Р·СЂСѓС‡РЅРѕСЃС‚С–)
     
-    cout << "Ситуація на даний момент:\n" << endl;
+    cout << "РЎРёС‚СѓР°С†С–СЏ РЅР° РґР°РЅРёР№ РјРѕРјРµРЅС‚:\n" << endl;
     cout << "-" << cells[0] << "-" << '|' << "-" << cells[1] << "-" << '|' << "-" << cells[2] << "-" << endl;
     cout << "-" << cells[3] << "-" << '|' << "-" << cells[4] << "-" << '|' << "-" << cells[5] << "-" << endl;
     cout << "-" << cells[6] << "-" << '|' << "-" << cells[7] << "-" << '|' << "-" << cells[8] << "-" << "\n" << endl;
@@ -21,11 +21,11 @@ void make_move(int num) {
     if (num == 1) cout << Player1;
     else cout << Player2;
     int player_move;
-    cout << ", виберіть номер комірки, зробіть свій хід - ";
+    cout << ", РІРёР±РµСЂС–С‚СЊ РЅРѕРјРµСЂ РєРѕРјС–СЂРєРё, Р·СЂРѕР±С–С‚СЊ СЃРІС–Р№ С…С–Рґ - ";
     cin >> player_move;
     while (player_move > 9 || player_move < 1 || cells[player_move - 1] == 'O' || cells[player_move - 1] == 'X')
     {
-        cout << "Введіть номер в діапазоні ( 1-9 ) або вільну ( --- ) комірку, щоб зробити хід:";
+        cout << "Р’РІРµРґС–С‚СЊ РЅРѕРјРµСЂ РІ РґС–Р°РїР°Р·РѕРЅС– ( 1-9 ) Р°Р±Рѕ РІС–Р»СЊРЅСѓ ( --- ) РєРѕРјС–СЂРєСѓ, С‰РѕР± Р·СЂРѕР±РёС‚Рё С…С–Рґ:";
         cin >> player_move;
     }
     cout << "\n";
@@ -40,18 +40,18 @@ char win_check()
         if (cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2])
             return cells[i];   //0-1-2, 3-4-5, 6-7-8
         else if (cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6])
-            return cells[i];  //0-3-6, 1-4-7 и 2-5-8
+            return cells[i];  //0-3-6, 1-4-7 Рё 2-5-8
         else if ((cells[2] == cells[4] && cells[4] == cells[6]) || (cells[0] == cells[4] && cells[4] == cells[8]))
-            return cells[i];   // 0-4-8 и 2-4-6
+            return cells[i];   // 0-4-8 Рё 2-4-6
     }
     return '-';
 }
 
 void result() {
     if (win == 'X')
-        cout << "Переміг гравець №1 - " << Player1 << endl;
-    else if (win == 'O') cout << "Переміг гравець №2 - " << Player2 << endl;
-    else if (win == '-') cout << "Нічия" << endl;
+        cout << "РџРµСЂРµРјС–Рі РіСЂР°РІРµС†СЊ в„–1 - " << Player1 << endl;
+    else if (win == 'O') cout << "РџРµСЂРµРјС–Рі РіСЂР°РІРµС†СЊ в„–2 - " << Player2 << endl;
+    else if (win == '-') cout << "РќС–С‡РёСЏ" << endl;
 }
 
 int main() {
@@ -59,10 +59,10 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    cout << "\tХрестики-нулики" << endl;
-    cout << "Імена гравців НЕ повинні бути однаковими" << endl;
-    cout << "Введите имя первого игрока : "; cin >> Player1;
-    cout << "Введите имя второго игрока: "; cin >> Player2;
+    cout << "\tРҐСЂРµСЃС‚РёРєРё-РЅСѓР»РёРєРё" << endl;
+    cout << "Р†РјРµРЅР° РіСЂР°РІС†С–РІ РќР• РїРѕРІРёРЅРЅС– Р±СѓС‚Рё РѕРґРЅР°РєРѕРІРёРјРё" << endl;
+    cout << "Р’РІРµРґРёС‚Рµ РёРјСЏ РїРµСЂРІРѕРіРѕ РёРіСЂРѕРєР° : "; cin >> Player1;
+    cout << "Р’РІРµРґРёС‚Рµ РёРјСЏ РІС‚РѕСЂРѕРіРѕ РёРіСЂРѕРєР°: "; cin >> Player2;
     cout << "\n";
 
     print_field();
